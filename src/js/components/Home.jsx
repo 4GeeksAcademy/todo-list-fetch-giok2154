1bb2a885debb719eb1e9e8ddb1d155cfe18857b3
Giovanny Cacua Pedraza <giovannycacua3@gmail.com>
2025-08-20 12:59:56 +0000

    estructura inicial

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,15 @@
-import React from "react";
+import { TodoListFetch } from "../TodoListFetch.jsx";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
+
+// Create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	// codigo JS
+	// console.log('estoy dentro del componente Home')
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	return (
+		<div className="d-flex flex-column min-vh-100 text-center">	
+			<TodoListFetch/>		
 		</div>
 	);
 };
